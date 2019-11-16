37566f49f0e9577cbdee0ff0e1d08172cdd062a6
wu000163 <wu000163@umn.edu>
2019-11-15 19:10:16 -0600

    Create candidate.h

---
+++ Project1/src/candidate.h
@@ -0,0 +1,14 @@
+#ifndef _CANDIDATE_H
+#define _CANDIDATE_H
+
+#include <iostream>
+using namespace std;
+
+class Candidate
+{
+private:
+    string name;
+    string party;
+    int votes;
+};
+#endif
