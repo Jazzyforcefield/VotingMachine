6556e44a861a635b7e78ea50f268a447dc021a4d
wu000163 <wu000163@umn.edu>
2019-11-18 21:08:40 -0600

    Update CPL.cc

--- Project1/src/CPL.cc
+++ Project1/src/CPL.cc
@@ -1,24 +1,29 @@
-// Copyright 2019, CSCI 5801
 
 #include "CPL.h"
 
-CPL::CPL(int seats, int ballots, int candidates, std::vector<Party *> *) {
+CPL::CPL(int seats, int ballots, int candidates ) {
   VotingType(seats, ballots, candidates);
-  
+
+
 }
 
-std::vector<Party *> * CPL::get_parties() {
-
+std::vector<Party*> * CPL::get_parties() {
+	return &parties_;
 }
 
 int CPL::increment(int index) {
+	int size_of_parties = parties_.size();
+
+	if (index > size_of_parties - 1 || index <0)
+	{
+		return -1;
+	}
+	
+	(*parties_[index]).votes++ ;
+    printf("%d\n",(*parties_[index]).votes);
+
+	return 0;
+
 
 }
 
-int CPL::display() {
-
-}
-
-int CPL::create_txt_file() {
-
-}
