17de126601c05e1f92f8a7186650cf856d92d2f4
wu000163 <wu000163@umn.edu>
2019-11-18 21:10:07 -0600

    Update VotingType.h

--- Project1/src/VotingType.h
+++ Project1/src/VotingType.h
@@ -1,7 +1,21 @@
 #ifndef _VOTINGTYPE_H
 #define _VOTINGTYPE_H
+#include "party.h"
+#include "candidate.h"
+#include "CPL.h"
+#include "OPL.h"
+#include <iostream>
+#include <vector>
+using namespace std;
 
-class VotingType {
- public:
-  
+class VotingType
+{
+public:
+	VotingType(int seats, int ballots, int candidates);
+	int increment(int index);
+private:
+	int seats_;
+	int ballots_;
+	int candidates_;
 };
+#endif
