cea55cc2d584b526406139c656342bd2c3ebaba1
bi000001 <bi000001@umn.edu>
2019-12-02 00:46:51 -0600

    ZhuoranBi20191201

--- Project2/src/CPL.h
+++ Project2/src/CPL.h
@@ -6,6 +6,10 @@
 #include <fstream>
 #include <cstdlib>
 #include <cmath>
+#include <map>
+#include <set>
+#include <ctime>
+#include <algorithm>
 
 #include "party.h"
 #include "candidate.h"
@@ -18,11 +22,14 @@ public:
 	std::vector<Party*>* get_parties();
 	int increment(int index);
 	int CalculateWinners();
+	double GenerateRandomNumber(unsigned int seed = std::time(NULL));
 	int create_txt_file();
 	int Display();
 
 private:
 	std::vector<Party*> parties_;
+	std::map<int, std::vector<int>> remainder_to_index_;
+	std::set<int, std::greater<int>> remainder_;
 };
 
 #endif
