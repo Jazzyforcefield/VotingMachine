c372045018ee4fee485d4ee6779a74896ead55a8
bi000001 <bi000001@umn.edu>
2019-12-02 00:49:22 -0600

    ZhuroanBi20191201

--- Project2/src/party.h
+++ Project2/src/party.h
@@ -9,11 +9,10 @@
 
 class Party {
  public:
-	Party(std::string party_name = "") { name_ = party_name; }
+	Party(std::string party_name = "") { name_ = party_name; seats_won_ = 0; votes_ = 0;}
 	std::string name_;
 	int votes_;
-	int seats_won_ = 0;
-	int remainder_ = 0;
+	int seats_won_;
 	std::vector<std::string> members_;
 };
 
