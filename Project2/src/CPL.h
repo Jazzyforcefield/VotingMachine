03108d75fcccd3fae11cf5e3269ad575ede9ce42
Michael Ung <ungxx028@umn.edu>
2019-12-06 17:57:59 -0600

    Updated project2

--- Project2/src/CPL.h
+++ Project2/src/CPL.h
@@ -1,3 +1,28 @@
+/**
+ * \class CPL
+ *
+ * \brief CPL class
+ *
+ * This class is CPL.  It is used for create a entity of CPL class inherited
+ * from VotingType. An entity of CPL class can have attributes like parties,
+ * remainder, remainder to index
+ * have methods like increment, get_parties, CalculateWinners, CalculateWinners
+ * GenerateRandomNumber, BreakTie, create_txt_file.
+ *
+ * \note Will be used in processor to create CPL entity
+ *
+ * \author Team25
+ *
+ * \version $Revision: 1.0 $
+ *
+ * \date $Date: 2019/12/01 $
+ *
+ * Contact: csci5801-fall2019-team25@umn.edu
+ *
+ * $structure id: #4 $
+ *
+ */
+
 #ifndef _CPL_H
 #define _CPL_H
 
@@ -18,17 +43,75 @@
 
 class CPL : public VotingType {
 public:
+	/**
+   * @brief Constructor of CPL
+   *
+   * @param seats An integer argument present seats
+   *
+   * @param ballots An integer argument present ballots
+   *
+   * @param candidates An integer argument present candidates
+   *
+   * @return Returns an entity of CPL class
+   **/
 	CPL(int seats, int ballots, int candidates);
+	/**
+   * @brief Get a list of parties of CPL
+   *
+   * @param NULL
+   *
+   * @return a vector of parties
+   **/
 	std::vector<Party*>* get_parties();
+	/**
+	 * @brief Increment the votes of candidates of index by one
+	 *
+	 * @param index an integer argument present index of candidate in vector
+	 *
+	 * @return Returns 0 represent success, -1 represent failure.
+	 **/
 	int increment(int index);
+	/**
+	 * @brief Calculate Winners of voting
+	 *
+	 * @param NULL
+	 *
+	 * @return Returns 0 represent success, -1 represent failure.
+	 **/
 	int CalculateWinners();
+	/**
+	 * @brief Generate random number with time seeds
+	 *
+	 * @param seed an int time seed
+	 *
+	 * @return a double random number
+	 **/
 	double GenerateRandomNumber(unsigned int seed = std::time(NULL));
+	/**
+	 * @brief Create txt file of resutls brief
+	 *
+	 * @param NULL
+	 *
+	 * @return Returns 0 represent success, -1 represent failure.
+	 **/
 	int create_txt_file();
 	int Display();
 
 private:
+	/**
+	* A private variable.
+	* Candidate vector candidates member.
+	*/
 	std::vector<Party*> parties_;
+	/**
+	* A private variable.
+	* map index and vector pair
+	*/
 	std::map<int, std::vector<int> > remainder_to_index_;
+	/**
+	* A private variable.
+	* set index and greater pair
+	*/
 	std::set<int, std::greater<int> > remainder_;
 };
 
--- Project2/src/processor.h
+++ Project2/src/processor.h
@@ -1,3 +1,26 @@
+/**
+ * \class processor
+ *
+ * \brief processor class
+ *
+ * This class is processor.  It is used for parsing input file and Calculate ballots
+ * An entity of processor class can have attributes like votes, filename
+ * have methods like process, parse_CPL_line, parse_OPL_line, get_one_index
+ * The main class of whole voting system
+ *
+ * \note Will be used to process the input file and generate the results
+ *
+ * \author Team25
+ *
+ * \version $Revision: 1.0 $
+ *
+ * \date $Date: 2019/12/01 $
+ *
+ * Contact: csci5801-fall2019-team25@umn.edu
+ *
+ * $structure id: #5 $
+ *
+ */
 #ifndef _PROCESSOR_H
 #define _PROCESSOR_H
 
@@ -13,14 +36,59 @@
 
 class Processor {
  public:
+   /**
+    * @brief Constructor of Processor
+    *
+    * @param string filename
+    *
+    * @return Returns an entity of Processor class
+    **/
   Processor(std::string fname = "");
+  /**
+   * @brief process the input file to generate audit file
+   *
+   * @param NULL
+   *
+   * @return Returns 0 represent success, -1 represent failure.
+   **/
   int process();
+  /**
+   * @brief parse OPL input file line by line
+   *
+   * @param candidate Candidate pointer
+   *
+   * @param line string line
+   *
+   * @return Returns 0 represent success, -1 represent failure.
+   **/
   int parse_OPL_line(Candidate * candidate, std::string line);
+  /**
+   * @brief parse CPL input file line by line
+   *
+   * @param line string line
+   *
+   * @return Returns 0 represent success, -1 represent failure.
+   **/
   int parse_CPL_line(std::string line);
+  /**
+   * @brief get the candidate index in the vector
+   *
+   * @param line string line
+   *
+   * @return index of the position of candidate in thier party
+   **/
   int get_one_index(std::string line);
-  
+
  private:
+   /**
+   * A private variable.
+   * VotingType pointer vote
+   */
   VotingType * vote_;
+  /**
+  * A private variable.
+  * String input filename
+  */
   std::string fname_;
 };
 
