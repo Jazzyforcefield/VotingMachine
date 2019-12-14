b17e92692da5f0dfeff956493053ca077825bf01
wu000163 <wu000163@umn.edu>
2019-12-13 21:38:43 -0600

    Add files via upload
    Add documentation for drivers file

--- Project2/testing/drivers/ScenarioDriver.cc
+++ Project2/testing/drivers/ScenarioDriver.cc
@@ -1,11 +1,40 @@
 // Copyright 2019, Team 25, CSCI 5801
-
+/**
+ * \class ScenarioDriver
+ *
+ * \brief ScenarioDriver
+ *
+ * This is the main() fucntion entry.  It is used for driving all the scenario
+ * The main() function is the entry for driving all scenario to run the test
+ * expected results and actual results of input file.
+ * The erorr file with fault file name are tested as well.
+ *
+ * \note Will be used to drive all scenario
+ *
+ * \author Team25
+ *
+ * \version $Revision: 1.0 $
+ *
+ * \date $Date: 2019/12/01 $
+ *
+ * Contact: csci5801-fall2019-team25@umn.edu
+ *
+ * $structure id: #14 $
+ *
+ */
 #include <iostream>
 #include <string>
 
 #include "src/processor.h"
-
+/**
+ * A public variable.
+ * integer verbose.
+ */
 int verbose;
+/**
+ * A public variable.
+ * string filename.
+ */
 std::string filename;
 
 int main(int argc, char ** argv) {
